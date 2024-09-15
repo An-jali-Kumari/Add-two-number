8c9cfe26b70b6b9adcc854c036614c6a1dbc33f5
Anjali Kumari <166202845+An-jali-Kumari@users.noreply.github.com>
2024-09-15 21:45:35 +0530

    Add files via upload

---
+++ Add two number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	int sum;
+	cout<<"Type a number:";
+	cin>>a;
+	cout<<"Type another number:";
+	cin>>b;
+	sum=a+b;
+	cout<<"Sum is:"<<sum;
+	return 0;
+}
